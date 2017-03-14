6922ea50ae8d21a57fe7b52349061d567e265c4f
Waldo Spek <waldospek@gmail.com>
2017-03-14 18:41:32 -0300

    Added logging for debugging timeout.

--- src/crawler.py
+++ src/crawler.py
@@ -7,6 +7,7 @@ import os
 import re
 import mechanize
 import logging
+import time
 from bs4 import BeautifulSoup
 from enum import Enum
 
@@ -243,6 +244,14 @@ class Rolodex(object):
         return composed_url
 
     def extract_contacts_from_html(self, html):
+        # Debug code for timeout bug
+        logger.debug("Attempting to extract contacts from HTML.")
+        file_path = '/media/waldo/DATA-SHARE/Code/prj_socialdata_backend/logs/html/' + time.strftime("%Y%m%d-%H%M%S") + '.txt'
+        logger.debug("File path: '{0}'.".format(file_path))
+        with open(file_path, 'w') as html_file:
+            html_file.write(html)
+            logger.debug("Wrote HTML to file '{0}'.".format(file_path))
+
         contacts = []
         soup = BeautifulSoup(html, "lxml")
         selection = soup.find_all('code')
